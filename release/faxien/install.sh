94079a0c39b4ee42680fcee3626cd5e8595391fb
Martin Logan <martinjlogan@erlware.org>
2008-02-17 22:11:57 -0600

    more preparations for R12B-1 testing

---
+++ release/faxien/install.sh
@@ -0,0 +1,46 @@
+#!/bin/sh
+
+# This file is meant to run in the base directory of the faxien package. It is not meant for development. 
+
+PREFIX="/usr/local/erlware"
+if [ "$1" != "" ]; then
+PREFIX="$1"
+fi
+
+if [ "$1" = "help" ] || [ "$1" = "--help" ];then
+    echo "just run install with no arguments to install to the default location"
+    echo "or run install <path> to install in a non standard location"
+    exit 0
+fi
+
+# Follow symlinks to the binary 
+echo $1 | sed -e 's;--;;' |  grep -e 'prefix=.*' > /dev/null
+if [ "$?" = "0" ];then
+	echo "$0 Error: use --prefix without an equals sign"
+	exit 1
+fi
+
+PROG_NAME="$0"
+BASE_DIR=`dirname $PROG_NAME`
+cd $BASE_DIR
+BASE_DIR=`pwd`
+cd -
+CONFIG_FILE=`ls $BASE_DIR/releases/*/faxien.config 2> /dev/null`
+FAX_DIR=$(dirname `ls $BASE_DIR/lib/faxien*/ebin/faxien.app 2> /dev/null`)
+uname | grep CYGWIN
+
+if [ "$?" = "0" ];then
+	CONFIG_FILE=`echo $CONFIG_FILE | sed -e 's;\/cygdrive\/.;;'`
+	FAX_DIR=`echo $FAX_DIR | sed -e 's;\/cygdrive\/.;;'`
+	BASE_DIR=`echo $BASE_DIR | sed -e 's;\/cygdrive\/.;;'`
+fi
+
+WILDCARD_DIRS="[\"$BASE_DIR/lib/*\"]"
+echo "PROG_NAME: $PROG_NAME"
+echo "BASE_DIR: $BASE_DIR"
+echo "FAX_DIR: $FAX_DIR"
+echo "CONFIG_FILE $CONFIG_FILE"
+echo "WILDCARD_DIRS $WILDCARD_DIRS"
+
+
+erl -pz $FAX_DIR -config $CONFIG_FILE -s fax_cmdln faxien_apply application load faxien -s fax_cmdln faxien_apply fax_util add_pzs $WILDCARD_DIRS -s ibrowse start -s fax_cmdln faxien_apply faxien install $BASE_DIR -s init stop -noshell -prefix $PREFIX
---
+++ release/faxien/smoke_test.sh
@@ -0,0 +1,48 @@
+#!/bin/sh
+
+# Thanks to Scott Parish for this tip. Halts the shell execution on 
+# the failure of a simple command (see sh SHELL GRAMMER for more).
+set -e
+
+echo " Starting the Faxien/Epkg Smoke Test"
+echo "====================================="
+echo ""
+
+faxien search cos 
+faxien installed #| head -n 5
+
+echo "describe the sinan application at its latest version"
+faxien describe_app sinan #| head -n 5
+
+faxien remove sinan
+faxien install sinan 0.8.6
+
+faxien outdated_apps
+echo "upgrade the sinan app"
+faxien upgrade_app sinan
+
+faxien outdated
+echo "upgrade all installed releases"
+faxien upgrade_all
+
+echo "add and remove publish repos"
+faxien add_publish_repo http://test_publish_repo.com
+faxien show_publish_repos
+faxien remove_publish_repo http://test_publish_repo.com
+faxien show_publish_repos
+
+echo "add and remove repos"
+faxien add_repo http://testrepo.com
+faxien show_repos
+faxien remove_repo http://testrepo.com
+faxien show_repos
+
+echo "show request timeout"
+faxien show_request_timeout
+echo "update request timeout to 99999"
+faxien set_request_timeout 99999
+echo "show request altered timeout"
+faxien show_request_timeout
+
+echo "show faxien and epkg versions"
+faxien version
