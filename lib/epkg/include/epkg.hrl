1449187995da113ab26bb1de021bd0e5f483d97d
Martin Logan <martinjlogan@Macintosh.local>
2009-03-17 20:00:07 -0500

    faxien ready for 5.6.5 and R13A

--- lib/epkg/include/epkg.hrl
+++ lib/epkg/include/epkg.hrl
@@ -7,18 +7,19 @@
 %% A static list of compiler versions associate with the erts vsns they are packaged with.  This list should be in descending
 %% order of erts version. 
 -define(COMPILER_VSN_TO_ERTS_VSN_TO_ERLANG_VSN, [
+				   {"4.6",   "5.7",   "R13A"},
 				   {"4.5.5", "5.6.5", "R12B-5"},
 				   {"4.5.4", "5.6.4", "R12B-4"},
 				   {"4.5.3", "5.6.3", "R12B-3"},
 				   {"4.5.2", "5.6.2", "R12B-2"},
 				   {"4.5.1", "5.6.1", "R12B-1"},
-				   {"4.5", "5.6", "R12B-0"},
+				   {"4.5",   "5.6",   "R12B-0"},
 				   {"4.4.5", "5.5.5", "R11B-5"},
 				   {"4.4.4", "5.5.4", "R11B-4"},
 				   {"4.4.3", "5.5.3", "R11B-3"},
 				   {"4.4.2", "5.5.2", "R11B-2"},
 				   {"4.4.1", "5.5.1", "R11B-1"},
-				   {"4.4", "5.5", "R11B-0"}
+				   {"4.4",   "5.5",   "R11B-0"}
 				  ]).
 
 %% Valid categories for the 'category' key in a control file.
